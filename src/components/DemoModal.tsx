5b9449ed68303dc4a15b4009f3e3ee86b508bace
Nhật Nguyễn <48057924+nhatnguyen9317@users.noreply.github.com>
2020-09-20 22:45:56 +0700

    up

---
+++ src/components/DemoModal.tsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import React from 'react';
+
+class DemoModal extends React.Component {
+    constructor (isOpen: boolean) {
+        this.state = {
+            isOpen: false
+        }
+    }
+}
