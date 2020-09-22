104110e88b2fcdcc27cf7e2d862a35a54672b804
Nhật Nguyễn <48057924+nhatnguyen9317@users.noreply.github.com>
2020-09-22 18:45:04 +0700

    commit demo modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import DemoModal from './components/DemoModal';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ComponentState {
+  isOpen: boolean;
 }
 
-export default App;
+export default class App extends React.Component<ComponentState> {
+  state: ComponentState = {
+    isOpen: false,
+  }
+
+  handleClick() {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={() => this.handleClick()}>Click here</button>
+        <DemoModal isOpen={this.state.isOpen} />
+      </>
+    );
+  }
+}
--- src/components/DemoModal.tsx
+++ src/components/DemoModal.tsx
@@ -1,10 +1,49 @@
-import { Component } from "react";
 import React from 'react';
+import './components-css/DemoModal.css';
 
-class DemoModal extends React.Component {
-    constructor (isOpen: boolean) {
-        this.state = {
-            isOpen: false
+interface ComponentProps {
+    isOpen: boolean;
+}
+
+interface ComponentStates {
+    isOpen: boolean;
+}
+
+export default class DemoModal extends React.Component<ComponentProps, ComponentStates> {
+
+    constructor(props: ComponentProps) {
+        super(props);
+        this.setState({ isOpen: this.props.isOpen });
+    }
+
+    modal() {
+        console.log("open modal")
+        return (
+            <div className="modal">
+                <div className="modal-head"></div>
+                <div className="modal-body"></div>
+            </div>
+        )
+    }
+
+    render() {
+        if (this.state.isOpen) {
+            return (
+                <>
+                    <div className="modal-background modal-open">
+                        <p className="button-close-modal">
+                            <i className="fas fa-times"></i>
+                        </p>
+                        {this.modal()}
+                    </div>
+                </>
+            );
+        }
+        if (!this.state.isOpen) {
+            return (
+                <>
+                </>
+            );
         }
     }
 }
