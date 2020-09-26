c26cd34db55c962a461ed78b2f8e2bde6f3410ae
Nhật Nguyễn <48057924+nhatnguyen9317@users.noreply.github.com>
2020-09-26 18:57:23 +0700

    commit start with react router

--- src/components/DemoModal.tsx
+++ src/components/DemoModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { closeModal } from '../actions/ModalToggleAction';
 import './components-css/DemoModal.css';
 
@@ -11,16 +11,22 @@ const DemoModal = () => {
     const isOpen = useSelector<State>(state => state.isOpen);
     const dispatch = useDispatch();
 
+    const Modal = () => {
+        return (
+            <div className="modal">
+                <div className="modal-head">
+                    <p className="modal-head-title">This is Modal</p>
+                    <p className="button-close-modal" onClick={() => dispatch(closeModal())}>&#x2716;</p>
+                </div>
+                <div className="modal-body"></div>
+            </div>
+        )
+    }
+
     if (isOpen) {
         return (
             <div className="modal-background modal-open">
-                <p className="button-close-modal" onClick={() => dispatch(closeModal())}>
-                    &#x2716;
-                </p>
-                <div className="modal">
-                    <div className="modal-head"></div>
-                    <div className="modal-body"></div>
-                </div>
+                <Modal />
             </div>
         );
     } else {
